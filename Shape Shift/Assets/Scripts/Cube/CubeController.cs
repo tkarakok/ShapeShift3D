5c76c94365b1d1c6f244ea9eee2e1c3be95337a8
Tunahan <59062912+tkarakok@users.noreply.github.com>
2021-12-14 16:40:57 +0300

    updated all system

--- Shape Shift/Assets/Scripts/Cube/CubeController.cs
+++ Shape Shift/Assets/Scripts/Cube/CubeController.cs
@@ -7,7 +7,7 @@ public class CubeController : MonoSingleton<CubeController>
     // define player properties
 
     #region Properties Move System
-
+    
     private float _speed = 3;
     #endregion
 
@@ -25,7 +25,7 @@ public class CubeController : MonoSingleton<CubeController>
             #region Cube Movement
             Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + _speed * Time.deltaTime);
             transform.position = newPosition;
-
+            
             #endregion
 
             // shift system for player and ghost object
